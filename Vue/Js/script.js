19ce42916893b661ddb91769459a4323b27ae081
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-11-04 09:59:55 +0100

    mod

--- Vue/Js/script.js
+++ Vue/Js/script.js
@@ -4,25 +4,22 @@ window.addEventListener("DOMContentLoaded", function () {
     const app = new Vue ({
         el: "#root",
         data:{
-            contacts: [
+            contacts:
+            [
                 {
                     name: 'Michele',
                     avatar: 'img/avatar_2.jpg',
                     messages: [{
-                        date: '10/01/2020 15:30:55',
+                        
                         text: 'Hai portato a spasso il cane?',
                         status: 'sent'
                          },
                         {
-                        date: '10/01/2020 15:50:00',
+                        
                         text: 'Ricordati di dargli da mangiare',
-                        status: 'sent'
+                        status: 'recived'
                         },
-                        {
-                        date: '10/01/2020 16:15:22',
-                        text: 'Tutto fatto!',
-                        status: 'received'
-                        }
+                     
                         ],
                         },
 
@@ -34,42 +31,33 @@ window.addEventListener("DOMContentLoaded", function () {
                     messages:
                      [  
                         {
-                        date: '20/03/2020 16:30:00',
                         text: 'Ciao come stai?',
                         status: 'sent'
                         },
                         {
-                        date: '20/03/2020 16:30:55',
+                        
                         text: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received'
                         },
-                        {
-                        date: '20/03/2020 16:35:00',
-                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
-                        }
+
                      ]},
-                {
-                name: 'Samuele',
-                avatar: 'img/avatar_4.jpg',
-                messages: 
-                 [
                     {
-                    date: '28/03/2020 10:10:40',
-                    text: 'La Marianna va in campagna',
-                    status: 'received'
+                    name: 'Samuele',
+                    avatar: 'img/avatar_4.jpg',
+                    messages: 
+                     [
+                        {
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
+                        },
+                        {
+                    
+                        text: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                        },
+                   
+                     ]
                     },
-                    {
-                    date: '28/03/2020 10:20:10',
-                    text: 'Sicuro di non aver sbagliato chat?',
-                    status: 'sent'
-                    },
-                    {
-                    date: '28/03/2020 16:15:22',
-                    text: 'Ah scusa!',
-                    status: 'received'
-                    }
-                ]},
 
                 {
                 name: 'Luisa',
@@ -77,22 +65,25 @@ window.addEventListener("DOMContentLoaded", function () {
                 messages: 
                 [
                     {
-                    date: '10/01/2020 15:30:55',
+                
                     text: 'Lo sai che ha aperto una nuova pizzeria?',
                     status: 'sent'
                     },
                     {
-                    date: '10/01/2020 15:50:00',
                     text: 'Si, ma preferirei andare al cinema',
                     status: 'received'
                     }
                 ],
-                },
-                ],
+
+            },
+            ],
+
+            newMsg:"",
+
             currentindex:0
             
         },
-
+// devo creare una funzione che al click di un determinato elemento 
         methods: {
             selezionaUtente(index){
                 this.currentindex = index
