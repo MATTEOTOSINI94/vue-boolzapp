7efcc6481cd109ac1b94933c6eecb982768443a8
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-11-05 15:54:28 +0100

    mod

--- Vue/Js/script.js
+++ Vue/Js/script.js
@@ -1,4 +1,6 @@
 Vue.config.devtools = true;
+dayjs.extend(window.dayjs_plugin_localizedFormat);
+dayjs.extend(window.dayjs_plugin_relativeTime);
 
 window.addEventListener("DOMContentLoaded", function () {
     const app = new Vue ({
@@ -14,14 +16,15 @@ window.addEventListener("DOMContentLoaded", function () {
                         {
                 
                         text: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        status: 'sent',
+                        date: dayjs().format('LLL'),
                         },
 
                         {  
                                          
                         text: 'Ricordati di dargli da mangiare',
-                        status: 'recived'
-
+                        status: 'recived',
+                        date: dayjs().format('LLL')
                         },
                      
                     ],
@@ -37,12 +40,14 @@ window.addEventListener("DOMContentLoaded", function () {
                     [  
                         {
                         text: 'Ciao come stai?',
-                        status: 'sent'
+                        status: 'sent',
+                        date: dayjs().format('LLL')
                         },
                         {
                         
                         text: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
+                        status: 'received',
+                        date: dayjs().format('LLL')
                         },
 
                     ]
@@ -56,12 +61,14 @@ window.addEventListener("DOMContentLoaded", function () {
                     [
                         {
                         text: 'La Marianna va in campagna',
-                        status: 'sent'
+                        status: 'sent',
+                        date: dayjs().format('LLL')
                         },
                         {
                     
                         text: 'Sicuro di non aver sbagliato chat?',
-                        status: 'received'
+                        status: 'received',
+                        date: dayjs().format('LLL')
                         },
                    
                     ]
@@ -77,33 +84,60 @@ window.addEventListener("DOMContentLoaded", function () {
                         {
                     
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        status: 'sent',
+                        date: dayjs().format('LLL')
                         },
                         {
                         text: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        status: 'received',
+                        date: dayjs().format('LLL')
                         }
                     ],
 
                 },
             ],
-            risposte:"ok",
+            risposte:["ok","tutto bene","a che ora ci vediamo?","a che ora?","va bene a domani",'Lo sai che ha aperto una nuova pizzeria?'],
             newMsg:"",
             activeChat:{},
+            filter:"",
+            arrayVuoto:[],
             
             
         },
 // devo creare una funzione che al click di un determinato elemento 
         methods: {
-            // metodo currentIndex
-            selezionaUtente(index){
-                this.currentindex = index
-                console.log(index)
-            },
+            // // metodo currentIndex
+            // selezionaUtente(index){
+            //     this.currentindex = index
+            //     console.log(index)
+            // },
              
+
+            // creo una funzione che al click mi vada a trasferire l'elemento designato nel mio arrat vuoto
             selectChat(el){
                 this.activeChat = el;
             },
+
+
+            lastElementList(messages){
+
+
+                if (messages.length === 0) {
+                    return "";
+                  }
+            
+                  const message = messages[messages.length - 1].text;
+            
+                  return message;
+                  
+                  //.slice(0, 20)
+                // const message = messages[messages.length - 1].text;
+
+                // return message; //.slice(0, 20)
+            },
+
+            // creo una funzione per stampare i messaggi  ovverò pusho la variabile impostata nell'input 
+            //  creo un'altra funzione all'interno  dove pusho la risposta dopo un determinato lasso di tempo
 
             senTMsg(){
                 if (this.newMsg === ""){
@@ -112,51 +146,58 @@ window.addEventListener("DOMContentLoaded", function () {
 
                 this.activeChat.messages.push({
                     text: this.newMsg,
-                    status:'sent'
+                    status:'sent',
+                    date: dayjs().format('LLL'),
                 })
                 this.newMsg = "";
-
 
                 setTimeout(() => {
                     // push del messaggio nell'array dei messaggi
                     this.activeChat.messages.push({
-                        text: this.risposte,
+                        text: this.risposte[Math.floor(Math.random() * this.risposte.length)],
                         status:"received",
+                        date: dayjs().format('LLL'),
                     })
                   }, 2000);
                 },
-  
+                 
+
+               
+                cancellaElemento(index){
+                    this.activeChat.messages.splice(index, 1)
+                },
+
+            // //   provo a creare una funzione per filtrare le parole
+                FilteredList() {
+                    return this.contacts.filter((elemento) => {
+                      return elemento.name .toLowerCase().includes(this.filter.toLowerCase());
+                    });
+                  },
+
+
+                  lastDate(date){
+
+                    if (messages.length === 0) {
+                    return "";
+                  }
+            
+                  const message = messages[messages.length - 1].date;
+            
+                  return message;
+
+                  }
             },
 
 
-            autoAnswer(){
-
-            },
-            onSubmitClick() {
-                // push del messaggio nell'array dei messaggi
-               
-        
-            // messaggiRicevuti(text,status){
-
-               
-            // },
-
-            // messaggiInviati(text,status){
-                
-            // }
+            beforeMount(){
+                this.activeChat = this.contacts[0]
+            }
 
 
 
-            // addMessage(testoMessaggio, stato) {
+            
 
-            //     this.activeChat.messages.push({
-
-            //       text: "",
-            //       date: "",
-            //       status: "",
-            //     });
-            //   },
-        }
+            
     })
 
 
